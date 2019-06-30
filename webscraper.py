f15b24bcc07449ecd421f7896a4b869012c32ca9
Shivansh Anand <shivansh586@gmail.com>
2019-06-30 16:24:13 +0530

    code made modular and flipkart added.

--- webscraper.py
+++ webscraper.py
@@ -4,40 +4,77 @@ import csv
 from os import path
 from bs4 import BeautifulSoup
 
-exitloop = True
-page = 1
-# use "+" instead of spaces to enter product_name
-product_name = "gift+box"
-fields = ["Product Name", "Image Url", "Product Url"]
-if not path.exists("amazon data.csv"):
-    f = open("amazon data.csv", "a")
-    writer = csv.writer(csvfile)
-    writer.writerow(fields)
-with open("amazon data.csv", 'a') as csvfile:
-    writer = csv.writer(csvfile)
+
+def get_url_data(site_url, params):
+    r = requests.get(site_url, params=params)
+    # print(r.status_code)
+    http_pool = urllib3.connection_from_url(r.url)
+    r = http_pool.urlopen('GET', r.url)
+    soup = BeautifulSoup(r.data, 'html.parser')
+    return soup
+
+
+def get_amazon_data(product_name):
+    exitloop = True
+    page = 1
     while exitloop:
-        r = requests.get('https://www.amazon.in/s', params={'k': product_name, 'page': page})
-        # print(r.status_code)
-        http_pool = urllib3.connection_from_url(r.url)
-        r = http_pool.urlopen('GET', r.url)
-        soup = BeautifulSoup(r.data, 'html.parser')
+        soup = get_url_data('https://www.amazon.in/s', {'k': product_name, 'page': page})
         count = 0
-        for img in soup.findAll("img", {"class": "s-image"}):
-                # Product
-            printimg = True
-            for parent in img.parents:
+        for product in soup.find_all("div", {"data-asin": True}):
+            sponsored = False
+            for tag in product.find_all('div'):
                 try:
-                    if parent["data-component-type"] == "sp-sponsored-result":
-                        printimg = False
+                    if tag["data-component-type"] == "sp-sponsored-result":
+                        sponsored = True
                         break
                 except:
                     pass
-            if printimg:
-                imghref = "https://amazon.in/" + img.find_parent("a")["href"]
-                data_list = [img["alt"], img["src"], imghref]
-                writer.writerow(data_list)
+            if not sponsored:
+                rating = 'No ratings Provided'
+                review = 'No responses available'
                 count += 1
-        print(count, page)
-        page += 1
+                img = product.find('img')
+                ahref = product.find('a')
+                ahref = 'https://www.amazon.in/'+ahref['href']
+                try:
+                    rnr = product.find('div', class_="a-row a-size-small")
+                    rating = rnr.find('span', class_='a-icon-alt').text
+                    review = rnr.find('span', class_='a-size-base').text
+                except:
+                    pass
+                yield {'Product Name': img['alt'], 'Image URL': img['src'], 'Product URL': ahref, 'Ratings': rating, 'No: of Responses': review}
         if count == 0:
             exitloop = False
+        print(count, page)
+        page += 1
+
+
+def get_flipkart_data(product_name):
+    exitloop = True
+    page = 1
+    while exitloop:
+        soup = get_url_data('https://www.flipkart.com/search', {'q': product_name, 'page': page})
+        try:
+            soup.find('div', {'data-id': True}).text
+            for product in soup.find_all('div', {'data-id': True}):
+                href = product.find('a', class_="Zhf2z-")['href']
+                href = 'https://www.flipkart.com'+href
+                img = product.find('img', {'class': "_1Nyybr"})  # Product Name,Image Url
+                rating = 'No ratings Provided'
+                responses = 'No responses available'
+                try:
+                    rating = product.find('div', class_='hGSR34').text
+                    responses = product.find('span', class_='_38sUEc').text[1:-1]
+                except:
+                    pass
+                yield {'Product Name': img['alt'], 'Image URL': img['src'], 'Product URL': href, 'Ratings': rating, 'No: of Responses': responses}
+            print(page)
+        except:
+            print('exception')
+            exitloop = False
+        page += 1
+
+
+if __name__ == '__main__':
+    # two functions created
+    pass
