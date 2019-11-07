9b195e308478d5cf08e2b213af0e337aa9a25251
Shivansh Anand <shivansh586@gmail.com>
2019-11-07 18:38:40 +0530

    pretty formatting.

--- file/views.py
+++ file/views.py
@@ -19,14 +19,52 @@ class Echo:
         return value
 
 
+def printCSV(data):
+    # try:
+    dataset = Dataset.objects.get(
+        name=data['Website'][0]+'-'+data['keyword'][0])
+    rows = []
+    fields = []
+    # reading csv file
+    with open(dataset.ml_file.path, 'r') as csvfile:
+        # creating a csv reader object
+        csvreader = csv.reader(csvfile)
+        fields = next(csvreader)
+        # extracting each data row one by one
+        count = 0
+        for row in csvreader:
+            count += 1
+            if count > 200:
+                break
+            rows.append(row)
+    return {'fields':fields,'rows':rows}
+    # except:
+    #     return None            
+
 def home(request):
     if request.method == 'POST':
-        form = WebsiteForm(request.POST)
-        if form.is_valid():
-            website_dict = form.cleaned_data
-            return redirect(f"/{website_dict['Website']}")
-    form = WebsiteForm()
-    return render(request, 'file/home.html', {'form': form})
+        pseudo_buffer = Echo()
+        writer = csv.writer(pseudo_buffer)
+        data = dict(request.POST)
+        print(data)
+        if data['Website'][0] == 'Amazon':
+            if data['category'][0] == '':
+                response = StreamingHttpResponse((writer.writerow(data) for data in get_amazon_data(data['keyword'][0], data['Country'][0])),
+                                                    content_type="text/csv")
+            else:
+                response = StreamingHttpResponse((writer.writerow(data) for data in get_amazon_data(data['keyword'][0], data['Country'][0], data['category'][0])),
+                                                    content_type="text/csv")
+            response['Content-Disposition'] = f"""attachment; filename="{data['Website']}-{data['keyword']}-{data['Country']}-{data['category']}.csv"""
+            return response
+        elif data['Website'][0] == 'Etsy':
+            EtsyCSVwriter.delay(data['keyword'][0])
+        elif data['Website'] == 'Alibaba':
+            AlibabaCSVwriter.delay(data['keyword'][0])
+        data = printCSV(data)    
+        # if printCSV(data) is None:
+        #     return render(request, "file/index.html", {"csv": False})
+        return render(request, 'file/index.html', {"csv": True,'rows': data['rows'], 'fields': data['fields']})
+    return render(request, 'file/index.html', {"csv": False})
 
 
 def website_form(request, website):
